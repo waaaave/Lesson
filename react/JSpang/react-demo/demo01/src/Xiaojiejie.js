a9a8531da3511e1a784c9bed51a18ebf524a673c
wave <1076544270@qq.com>
2021-10-13 18:55:12 +0800

    react学习

--- react/JSpang/react-demo/demo01/src/Xiaojiejie.js
+++ react/JSpang/react-demo/demo01/src/Xiaojiejie.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import './style.css'
+import XiaojiejieItem from "./XiaojiejieItem";
 
 class Xiaojiejie extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            inputValue: '请输入要添加的服务',
-            list: []
+            inputValue: '',
+            list: ['合肥推拿','老衲冲刺']
         }
     }
 
@@ -13,26 +15,59 @@ class Xiaojiejie extends Component {
         return (
             <div>
                 <div>
-                    <input value={this.state.inputValue} onChange = {this.inputChange.bind(this)} />
-                    <button>增加服务</button>
+                    <label htmlFor="addl">增加服务：</label>
+                    <input id="addl" className="input" value={this.state.inputValue} onChange = {this.inputChange.bind(this)} />
+                    <button onClick={this.addList.bind(this)}>增加服务</button>
                     </div>
                 <ul>
-                    <li>港派指压</li>
-                    <li>精油护体</li>
-                    <li>足底按摩</li>
-                    <li>请他吃刀削</li>
+                    {
+                        this.state.list.map((item,index)=>{
+                            return (
+                                {/**
+                                <li 
+                                 onClick ={this.deleteItem.bind(this,index)}
+                                 dangerouslySetInnerHTML = {{__html:item}}
+                                 key={index + item}>
+                                 
+                             </li> */},
+                                 <XiaojiejieItem
+                                 key={index + item}
+                                 index = {index}
+                                 deleteItem = {this.deleteItem.bind(this)}
+                                 content={item} />
+
+                            )
+                        })
+                    }
                 </ul>
             </div>
         )
     }
 
     inputChange(e){
-        console.log(e.target.value);
+        // console.log(e.target.value);
         this.setState({
             inputValue:e.target.value
         })
 
     }
+    //删除列表项
+    deleteItem(index){
+        let list = this.state.list
+        list.splice(index,1)
+        this.setState({
+            list:list
+        })
+    }
+
+    //增加列表
+    addList(){
+        this.setState({
+            list:[...this.state.list,this.state.inputValue],
+            inputValue: ''
+        })
+    }
+
 }
 
 export default Xiaojiejie
---
+++ react/JSpang/react-demo/demo01/src/XiaojiejieItem.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+class XiaojiejieItem extends Component {
+    constructor(props){
+        super(props)
+        this.handleClick = this.handleClick.bind(this)
+    }
+    render() { 
+        return (
+            <li onClick={this.handleClick.bind(this)}>{this.props.content}</li>
+        );
+    }
+    handleClick(){
+        this.props.deleteItem(this.props.index)
+    }
+}
+ 
+export default XiaojiejieItem;
