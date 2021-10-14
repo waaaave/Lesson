8f97681b2b9e458f2b757a8e23b161f1b1f56c7b
wave <1076544270@qq.com>
2021-10-14 15:18:10 +0800

    一点点算法练习

--- leetcode/self/1.js
+++ leetcode/self/1.js
@@ -1,22 +1,24 @@
-var plusOne = function(digits) {
+var plusOne = function (digits) {
     let n = 1;
-    var add=function(n){
-        if(digits[digits.length-n]==9){
-            n+=1;
-            add(n);
-        }else{
-            if(n==1){
-                digits[digits.length-n] +=1;
-            }else{
-                digits[digits.length-n] +=1;
-                for(n;n>1;n--){
-                    digits[digits.length-n]=0;
+    var add = function (digits) {
+        if (digits[digits.length - n] == 9) {
+            n++
+            add(digits);
+        } else {
+            if (n == 1) {
+                digits[digits.length - n] += 1;
+            } else {
+                digits[digits.length - n] += 1;
+                for (let i = (n - 1); i > 0; i--) {
+                    digits[digits.length - i] = 0;
                 }
             }
 
         }
     }
+    add(digits);
     return digits;
-};
 
-console.log(plusOne([1,2,3]));
+
+};
+console.log(plusOne([9]));
