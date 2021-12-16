8f60970367b2cb51f83cd2a256724f72de8c42bf
wave <1076544270@qq.com>
2021-12-16 17:19:50 +0800

    移动 0 算法练习

--- leetcode/move0/1.js
+++ leetcode/move0/1.js
@@ -1,19 +1,14 @@
 var moveZeroes = function(nums) {
-  var n = nums.length
-  
-  var change1 = (nums) => {
-      for(let i = n; i > 0; i--){
-      if(nums[i] == 0 && i!=n-1){
-          nums[i]= nums[i+1]
-          nums[i+1] = 0
-          change1(nums)
-      }
-      if(nums[n] == 0){
-          n--
-      }
-      
-  }
-  }
-  change1(nums)
-  
+    let j = 0;
+    for(let i = 0; i< nums.length; i++){
+        if(nums[i] != 0){
+            nums[j] = nums[i];
+            j++
+        }
+    }
+    for(let i = j; i<nums.lenth; i++){
+        nums[i] = 0
+    }
 };
+
+moveZeroes([0,1,0,3,12])
