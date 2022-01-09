b2f38b190082626a5abc46e5db8bd57bbfdcd42b
wave <1076544270@qq.com>
2022-01-09 20:59:33 +0800

    部署上线

--- zan/mall/src/App.jsx
+++ zan/mall/src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import {Provider} from 'react-redux';
 import store from './store/index';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import {renderRoutes} from 'react-router-config';
 import routes from './routes/index';
 
@@ -11,9 +9,9 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <HashRouter>
+        <BrowserRouter>
           {renderRoutes(routes)}
-        </HashRouter>
+        </BrowserRouter>
       </div>
     </Provider>
   )
--- zan/readme.md
+++ zan/readme.md
@@ -9,4 +9,20 @@
     react + ts + node + 性能优化 + 组件
 
   - 项目上线服务器
-    1. gitee 配置 gitee pages 服务
+    1. gitee 配置 gitee pages 静态文件托管服务
+    如果我们的项目是单纯的react项目（后端有第三方提供接口或不需要）
+    npm run build 静态文件 
+
+    2. 买服务器
+      1. putty 或其他 链接服务器
+      2. filezilla ftp上传文件服务 xshell
+      3. 服务器上运行
+
+1. 有一台服务器
+2. 把 mall npm run build dist/
+3. server 目录删除 node_modules
+4. putty  /var 目录下 mkdir /www
+5. final shell 链接服务器 ftp 端口
+6. 确保服务器开放了22 端口
+7. /ww/mall/dist(server)
+
